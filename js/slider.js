9d08f34db3ca8c133ba47f2999c35f8dba44da97
Hasibul Polok <hasibulpolok.bdn@gmail.com>
2021-09-14 01:24:09 +0600

    simple js slider

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+const images = [
+    'img/pic-1.jpg',
+    'img/pic-2.jpg',
+    'img/pic-3.jpg',
+    'img/pic-4.jpg'
+];
+
+const displayImage = document.getElementById('slider-img');
+
+let imgIndex = 0;
+
+setInterval(() => {
+
+    if (imgIndex >= images.length) {
+        imgIndex = 0;
+    };
+    const imgUrl = images[imgIndex];
+    imgIndex++;
+    displayImage.setAttribute('src', imgUrl);
+
+}, 2000);
