cc2fff2b3bc0a0da5ce0698fd190560276b827b5
Hasibul Polok <hasibulpolok.bdn@gmail.com>
2021-09-14 01:44:50 +0600

    new slider image added

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,8 @@ const images = [
     'img/pic-1.jpg',
     'img/pic-2.jpg',
     'img/pic-3.jpg',
-    'img/pic-4.jpg'
+    'img/pic-4.jpg',
+    'img/pic-5.jpg',
 ];
 
 const displayImage = document.getElementById('slider-img');
